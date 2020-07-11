6742d3b029484d5d1b6822364a6f7036541f2a0a
ChunGyi-j <moethuchun@gmail.com>
2020-07-11 12:17:33 +0630

    first commit

---
+++ src/main/java/com/spring/registration/model/repo/RegisterRepo.java
@@ -0,0 +1,18 @@
+package com.spring.registration.model.repo;
+
+import com.spring.registration.model.entity.Register;
+import com.spring.registration.model.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RegisterRepo extends JpaRepository<Register,Long> {
+    List<Register> findByEnable(boolean enable);
+   List<Register> findByStudentId(Long id);
+   Register findByStudentIdAndCourseId(Long studentId,Long courseId);
+   List<Register> findByCourseIdAndEnable(Long courseId,boolean enable);
+   List<Register> findByLook(boolean look);
+   List<Register> findByCourseId(Long id);
+
+
+}
---
+++ src/main/java/com/spring/registration/model/repo/TokenRepo.java
@@ -0,0 +1,10 @@
+package com.spring.registration.model.repo;
+
+import com.spring.registration.model.entity.Account;
+import com.spring.registration.model.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepo extends JpaRepository<Token,Long> {
+    Token findByToken(String token);
+
+}
